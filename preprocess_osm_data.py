8ef048e9ba31055be2b3a47c5371b89f5f9f572d
Derek Lin <dyl14@sfu.ca>
2020-11-23 12:25:17 -0800

    initial commit for osm data preprocessor - fill in missing wikidata identifiers where applicable

---
+++ preprocess_osm_data.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import sys
+
+def main(input_file, output_file):
+    """Preprocess OSM data by filling in wikidata identifiers (qid)
+    for OSM entries that have a Wikidata entry but are not associated with one.
+        - write preprocessed OSM data to output_file
+
+    Args:
+        input_file (str):
+            osm_output_file from scrape_wikidata.py - should include the data
+            from amenities-vancouver.json.gz along with an additional column
+            for qid
+            
+            
+        output_file (str):
+            json file in which some qids have been filled in
+    """
+
+    osm_data = pd.read_json(input_file)
+
+    has_qid = osm_data['qid'].notna()
+    has_name = osm_data['name'].notna()
+
+    # create dict with (name, qid) key value pairs - dict zip adapted from
+    # Wouter Overmeire's answer at https://stackoverflow.com/questions/17426292/
+    valid_name_qid = osm_data.loc[has_qid & has_name, ['name', 'qid']].copy()
+    name_qid = dict(zip(valid_name_qid['name'], valid_name_qid['qid']))
+
+    # fill in missing qids for OSM data that have a Wikidata entry
+    osm_data['mapped_qid'] = osm_data['name'].map(name_qid)
+
+    # set mapped_qid of OSM data that have a qid but do not have a name
+    # equal to qid 
+    osm_data.loc[has_qid & ~has_name, 'mapped_qid'] = (
+        osm_data.loc[has_qid & ~has_name, 'qid']
+    )
+
+    # qid and mapped_qid for OSM data associated with a Wikidata entry should
+    # be the same
+    assert (
+        all(osm_data.loc[has_qid, 'qid'] == osm_data.loc[has_qid, 'mapped_qid'])
+    )
+
+    # replace qid column values with mapped_qid
+    preprocessed_osm_data = osm_data.drop(['qid'], axis=1)
+    preprocessed_osm_data.rename(columns={'mapped_qid': 'qid'}, inplace=True)
+    preprocessed_osm_data.to_json(output_file)
+    
+if __name__ == '__main__':
+    main(sys.argv[1], sys.argv[2])
