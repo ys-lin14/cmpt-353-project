531a09653874baf552c03fd4746581287507ba6e
Derek Lin <dyl14@sfu.ca>
2020-11-24 23:26:10 -0800

    rename plot_word_counts to plot_ngram_counts, add argument for plot title, update variable names, add docstrings

--- visualize.py
+++ visualize.py
@@ -22,20 +22,29 @@ def plot_wordcloud(text):
         height=1200,
         collocations=False # exclude bigrams
     ).generate(text)
-    
-    plt.figure(figsize=(20, 10)) # adjust overall size of wordcloud
+
+    # adjust overall size of wordcloud
+    plt.figure(figsize=(20, 10))
     plt.imshow(wordcloud, interpolation='bilinear')
     plt.axis('off')
     plt.show()
 
-def plot_word_counts(word_counts, wikidata_label):
-    num_words = word_counts.shape[0]
-    word_count_plot = sns.barplot(data=word_counts, x='word', y='count')
-    word_count_plot.set(
-        title=f'Top {num_words} Words in Wikidata {wikidata_label}', 
-        xlabel='Word', 
-        ylabel='Count'
-    )
-    xlabels = word_count_plot.get_xticklabels()
-    word_count_plot.set_xticklabels(xlabels, rotation=30, ha='right')
+def plot_ngram_counts(ngram_counts, title):
+    """Plot n-grams along with their counts
+
+    Args:
+        ngram_counts (dataframe):
+            n-grams along with their counts in the columns 'ngram' and 'count'
+
+        title (str): title of barplot for n-gram counts
+
+    Returns:
+        None
+    """
+    
+    num_ngram = ngram_counts.shape[0]
+    ngram_count_plot = sns.barplot(data=ngram_counts, x='ngram', y='count')
+    ngram_count_plot.set(title=title, xlabel='N-gram', ylabel='Count')
+    xlabels = ngram_count_plot.get_xticklabels()
+    ngram_count_plot.set_xticklabels(xlabels, rotation=30, ha='right')
     plt.show()
